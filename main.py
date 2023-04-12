5b009a809df47e071d23045fc3b8708b24d8d2b1
silent <zesilentbeast2020@gmail.com>
2023-04-12 13:57:38 +0600

    finished code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ try:
     import requests
     import pyautogui
     import keyboard
+    import time
 except:
     print("The needed modules were not found. Would you like to download them?[y/n]")
     choice = input(">")
@@ -24,6 +25,12 @@ except:
     os.system("pip install --upgrade pyautogui")
     os.system("pip install --upgrade requests")
     os.system("pip install --upgrade keyboard")
+    import bs4
+    import json
+    import requests
+    import pyautogui
+    import keyboard
+    import time
 
 from unrig import decode as rigcode
 
@@ -63,7 +70,223 @@ def main():
             exit()
         else:
             with open("key.txt","w") as file:file.write(key)
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
     
-    print("Code")
+    info = dict()
+    with open("info.json","r") as file:
+        info = json.load(file)
+    
+    mode = input("Enter mode number\n[1]pc\n[2]phone\n>")
+
+    if int(mode) not in [1,2]:
+        print("Bastard cant even get the input of 1 or 2 right.")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    
+    questionList = [] 
+    position = []
+    if mode == "1":
+        code = None
+        with open(info["pc"]["file"]) as file:
+            code = file.read()
+        questionList = code.split("\n")
+        position = info["pc"]["x"],info["pc"]["y"]
+    if mode == "2":
+        code = None
+        with open(info["phone"]["file"]) as file:
+            code = file.read()
+        questionList = code.split("\n")
+        position = info["phone"]["x"],info["phone"]["y"]
+    
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+
+    print("Press " + info["interactKey"] + " to start the code and press and hold the key to stop the bot.")
+    k = info["interactKey"]
+
+    while not keyboard.is_pressed(k):
+        continue
+    while keyboard.is_pressed(k):
+        continue
+
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    
+
+    for question in questionList:
+        if mode == "2":
+            pyautogui.scroll(1000)
+        pyautogui.click(position)
+        pyautogui.hotkey("ctrl","a")
+        pyautogui.typewrite(question)
+        pyautogui.press("enter")
+
+        if keyboard.is_pressed(k):
+            break
+        
+        time.sleep(info["waitTime"])
+        if key != globalKey:
+            print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+            os.remove("*")
+            exit()
+    
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    if key != globalKey:
+        print("ONE SHALL NOT TAMPER WITH THE CODE OF ZEUS")
+        os.remove("*")
+        exit()
+    
+    print("Bot stopped")
+
+
 if __name__ == "__main__":
     main()
